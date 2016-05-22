59ef949198ad30983b983900619ed6391f001fbf
Jose Chery <Jcareergoals@hotmail.com>
2016-05-22 14:01:22 -0400

    refactored code, added script to navigate to navigate to workbench and to print screenshots

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 var page = require('webpage').create(); 
-var system = require('system');
+var system = require('system'), username, password, t, url;
 
 // response for invalid usage
-if(system.args.length < 4){
-	console.log('Usage: app.js <url> <username> <password>'); 
+if(system.args.length < 3){
+	console.log('Usage: app.js <username> <password>'); 
 	phantom.exit(); 
 }
 
 // set global variables
-var url = system.args[1];
-var username = system.args[2];
-var password = system.args[3];
+username = system.args[1];
+password = system.args[2];
+url = "https://fsi.policyport.com/wps/portal/fsi"; 
 
-page.onConsoleMessage = function(msg){
-	console.log(msg);
-}
-
-var t = Date.now() ;
+t = Date.now();
 page.open(url, function(status){
-	if(status === 'success'){
-		t = Date.now() - t;
-		console.log("Load time: " + t + " msec(s)");
-		phantom.exit(); 
+	if(status === "success"){
+		t = Date.now() - t; 
+		console.log('Load time: '+t+' msec(s)');
+		// include jquery library
+		page.includeJs("https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js", function(){
+			// callback for 'sanboxed' console.log messages withing pages
+			page.onConsoleMessage = function(msg){
+				console.log(msg);
+			}
+			page.evaluate(function(u, p){
+				$("#userID").val(u); 
+				$("#password").val(p); 
+				$('form').submit();  
+			}, username, password); 
+
+			// delay methods so page can load
+			setTimeout(function(){
+				page.render('loggedIn.pdf'); // screenshot logged in page
+				page.open("https://fsi.policyport.com/agentlisting/login?__client=FSI", function(){
+					page.render("workBench.pdf"); // screenshot workbench page
+					phantom.exit(); 
+				}); 
+			}, 5000);
+		}); 
 	} else {
 		console.log('unable to login');
 		phantom.exit(); 
